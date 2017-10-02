0a078d600fd35f52b2f5017d1e788b71d51f1456
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-02 11:56:09 +0300

    Fix prints 2

--- XmlModule.py
+++ XmlModule.py
@@ -1,27 +1,31 @@
 class ConfigModule(object):
     from re import search
 
-    list_of_keys = []
-    list_of_active_keys = []
-    header = ''
-    list_of_keys_to_print = []
+    #    list_of_keys = []
+    #    list_of_active_keys = []
+    #    header = ''
+    #    list_of_keys_to_print = []
 
+    # check all files in config directory and return dictionary of keys
     def get_dit_from_files(self):
         from os import listdir
         import xml.etree.cElementTree as ET
+
         dict_of_keys = {}
         list_of_files = listdir("config")
+
         for i in list_of_files:
             try:
                 i = i.replace("[", "").replace("]", "").replace("'", "")
-                print(i)
                 tree = ET.ElementTree(file="config\\" + i)
                 list_of_keys = str(tree.findtext('KEYS')).split(' ')
+
                 dict_of_keys[i] = list_of_keys
-                return dict_of_keys
             except FileNotFoundError:
                 dict_of_keys = {}
                 return dict_of_keys
+
+        return dict_of_keys
 
 
     def work_with_xml(self):
