f75cf8980051b5de2bf7a20f1806d7269b467fed
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-02 08:23:07 +0300

    Commit

--- XmlModule.py
+++ XmlModule.py
@@ -1,12 +1,24 @@
 from re import *
+import os
 import xml.etree.cElementTree as ET
 
-tree = ET.ElementTree(file = config.xml)
-print tree.getroot()
-print type(tree.getroot())
 
-file = open("New Text Document.txt","r")
-text = file.read()
+class ConfigModule(object):
 
-file_rxbsp = open("rxbsp.txt","r")
-text_rxbsp = file_rxbsp.read()
+ print(os.listdir("config"))
+    # connect to config file
+ list_of_keys = []
+ list_of_active_keys = []
+ header = ''
+ list_of_keys_to_print = []
+ try:
+     tree = ET.ElementTree(file="config\\rxbsp.xml")
+ except FileNotFoundError:
+        print("File not found")
+
+ list_of_keys = str(tree.findtext('ALL_KEYS')).split(' ')
+ print(list_of_keys)
+
+    # get list of keys from config file
+ def work_with_xml(self, tree):
+     pass
