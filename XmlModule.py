2cdb7d9ec82a0ff70085286307606075faee166d
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-02 12:45:05 +0300

    add function that return dict with keys and types

--- XmlModule.py
+++ XmlModule.py
@@ -1,13 +1,12 @@
 class ConfigModule(object):
     from re import search
-
-    #    list_of_keys = []
-    #    list_of_active_keys = []
-    #    header = ''
-    #    list_of_keys_to_print = []
+    list_of_keys = []
+    ist_of_active_keys = []
+    header = ''
+    list_of_keys_to_print = []
 
     # check all files in config directory and return dictionary of keys
-    def get_dit_from_files(self):
+    def get_dict_from_files(self):
         from os import listdir
         import xml.etree.cElementTree as ET
 
@@ -18,22 +17,23 @@ class ConfigModule(object):
             try:
                 i = i.replace("[", "").replace("]", "").replace("'", "")
                 tree = ET.ElementTree(file="config\\" + i)
-                list_of_keys = str(tree.findtext('KEYS')).split(' ')
 
+                list_of_keys = str(tree.findtext('KEYS')).split(' ')
                 dict_of_keys[i] = list_of_keys
+
             except FileNotFoundError:
                 dict_of_keys = {}
                 return dict_of_keys
-
         return dict_of_keys
-
-
-    def work_with_xml(self):
+#function that return keys with their types
+    def get_dict_with_properties(self, true_config):
         import xml.etree.cElementTree as ET
-        tree = ET.ElementTree(file="config\\" + self)
-        print(tree)
-        for item in tree.iterfind('.ACTIVE_KEYS//'):
-            # print('Find: %s' % item.tag)
-            # print(item.items)
+        dict_type_of_keys = {}
+        tree = ET.ElementTree(file="config\\" + true_config)
+        for item in tree.iterfind('.ACTIVE_KEYS/'):
+            dict_type_of_keys[item.tag] = (item.attrib).get("type")
+            print(item.attrib)
             print(item.tag)
-            print(item.text)
+        return dict_type_of_keys
+
+
