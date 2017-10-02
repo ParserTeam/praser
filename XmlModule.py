e8720bb098b1a0b80c207af18aba23a5a72c4e1b
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-02 11:52:36 +0300

    Fix prints

--- XmlModule.py
+++ XmlModule.py
@@ -1,20 +1,11 @@
 class ConfigModule(object):
     from re import search
-    # connect to config directory
-
 
     list_of_keys = []
     list_of_active_keys = []
     header = ''
     list_of_keys_to_print = []
 
-    # open print_out
-    # file = open("rxbsp.txt", "r")
-    # print_out = file.read()
-    # #print(print_out)
-    # file.close()
-    #
-    # find true config file
     def get_dit_from_files(self):
         from os import listdir
         import xml.etree.cElementTree as ET
@@ -27,14 +18,11 @@ class ConfigModule(object):
                 tree = ET.ElementTree(file="config\\" + i)
                 list_of_keys = str(tree.findtext('KEYS')).split(' ')
                 dict_of_keys[i] = list_of_keys
-                #dict_of_keys = {}
-
-                #dict_of_keys = dict_of_keys + dict_of_keys2
-               # print(dict_of_keys)
-
+                return dict_of_keys
             except FileNotFoundError:
-                print("File with config not found")
-        return dict_of_keys
+                dict_of_keys = {}
+                return dict_of_keys
+
 
     def work_with_xml(self):
         import xml.etree.cElementTree as ET
@@ -45,7 +33,3 @@ class ConfigModule(object):
             # print(item.items)
             print(item.tag)
             print(item.text)
-
-
-object1 = ConfigModule()
-print(object1.get_dit_from_files())
