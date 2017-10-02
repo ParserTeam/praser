a0825c86aedf41b63b1e5a3c08b7908ea39f8cbc
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-02 11:22:14 +0300

    add dict of keys

--- XmlModule.py
+++ XmlModule.py
@@ -1,52 +1,41 @@
 class ConfigModule(object):
-    import os
-    import xml.etree.cElementTree as ET
     from re import search
     # connect to config directory
-    list_of_files = os.listdir("config")
+
 
     list_of_keys = []
     list_of_active_keys = []
     header = ''
     list_of_keys_to_print = []
+
     # open print_out
-    file = open("rxbsp.txt", "r")
-    print_out = file.read()
-    print(print_out)
-    file.close()
-
+    # file = open("rxbsp.txt", "r")
+    # print_out = file.read()
+    # #print(print_out)
+    # file.close()
+    #
     # find true config file
-    for i in list_of_files:
-        try:
-            i = i.replace("[", "").replace("]", "").replace("'", "")
-            tree = ET.ElementTree(file="config\\" + i)
-            list_of_keys = str(tree.findtext('ALL_KEYS')).split(' ')
-
-            regular = r''
-            for j in list_of_keys:
-                regular = regular + str(j) + ".*"
-            regular = regular + "\\n"
-            check = search(regular, print_out)
+    def get_dit_from_files(self):
+        from os import listdir
+        import xml.etree.cElementTree as ET
+        list_of_files = listdir("config")
+        for i in list_of_files:
             try:
-                if type(check.group()) != "NoneType":
-                    true_config = i
-            except AttributeError:
+                i = i.replace("[", "").replace("]", "").replace("'", "")
+                tree = ET.ElementTree(file="config\\" + i)
+                list_of_keys = str(tree.findtext('ACTIVE_KEYS')).split(' ')
+                dict_of_keys = {i: list_of_keys}
+
+            except FileNotFoundError:
                 print("File with config not found")
+        return dict_of_keys
 
-        except FileNotFoundError:
-            print("File with config not found")
-
-    print(true_config)
-
-    # get list of keys from config file
     def work_with_xml(self):
         import xml.etree.cElementTree as ET
         tree = ET.ElementTree(file="config\\" + self)
         print(tree)
         for item in tree.iterfind('.ACTIVE_KEYS//'):
-            #print('Find: %s' % item.tag)
-            #print(item.items)
+            # print('Find: %s' % item.tag)
+            # print(item.items)
             print(item.tag)
             print(item.text)
-
-    work_with_xml(true_config)
