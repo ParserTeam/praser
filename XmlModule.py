817a3893427052d706c30e9760334269b039e023
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-03 13:32:56 +0300

    fix prints

--- XmlModule.py
+++ XmlModule.py
@@ -13,6 +13,7 @@ class ConfigObject:
     norm_val = ''
     dict_type_of_keys = {}
     dict_bits = {}
+
     #
     # def __init__(self, name=None, text_of_bit=None, value=None):
     #     self.name = name
@@ -20,13 +21,15 @@ class ConfigObject:
     #     self.text_of_bit = text_of_bit
 
     def __str__(self):
-        return (str(self.name_key) + " " + str(self.header) + " " + str(self.root_limiter)+ " " + str(self.active_key_limiter)+ " " + str(self.dict_bits))
+        return (str(self.name_key) + " " + str(self.header) + " " + str(self.root_limiter) + " " + str(
+            self.active_key_limiter) + " " + str(self.dict_bits))
+
     def __repr__(self):
         return self.__str__()
 
 
 class ConfigModule(object):
-    #key_tag = ['root','HEDER','NAME_KEY','ACTIVE_KEYS']
+    # key_tag = ['root','HEDER','NAME_KEY','ACTIVE_KEYS']
     list_of_keys = []
     list_of_active_keys = []
     dict_bits = {}
@@ -49,39 +52,39 @@ class ConfigModule(object):
                 return {}
         return dict_of_keys
 
-
     # function that return keys with their types
     def get_list_objects(self, true_config):
-       list_of_objects = {}
-#run by list of files
-       for i in true_config:
-#create a empty object of file
-         file_object = ConfigObject()
-#open xml config file
-         tree = ET.ElementTree(file="config\\" + i)
-         file_object.header=tree.find('HEDER').text
-         file_object.name_key = tree.find('NAME_KEY').text
-         file_object.root_limiter = tree.getroot().attrib.get("limiter")
-         file_object.active_key_limiter = tree.find('ACTIVE_KEYS').attrib.get("limiter")
+        list_of_objects = {}
+        # run by list of files
+        for i in true_config:
+            # create a empty object of file
+            file_object = ConfigObject()
+            # open xml config file
+            tree = ET.ElementTree(file="config\\" + i)
+            file_object.header = tree.find('HEDER').text
+            file_object.name_key = tree.find('NAME_KEY').text
+            file_object.root_limiter = tree.getroot().attrib.get("limiter")
+            file_object.active_key_limiter = tree.find('ACTIVE_KEYS').attrib.get("limiter")
 
-         for item in tree.iterfind('.ACTIVE_KEYS/'):
-            file_object.dict_type_of_keys[item.tag] = (item.attrib).get("type")
-            for bits in item:
-                bit_object = ConfigObject()
-                bit_object.name = bits.tag
-                bit_object.text_of_bit = bits.text
-                bit_object.value = bits.attrib.get("value")
+            for item in tree.iterfind('.ACTIVE_KEYS/'):
+                file_object.dict_type_of_keys[item.tag] = (item.attrib).get("type")
+                for bits in item:
+                    bit_object = ConfigObject()
+                    bit_object.name = bits.tag
+                    bit_object.text_of_bit = bits.text
+                    bit_object.value = bits.attrib.get("value")
 
-                if item.tag in file_object.dict_bits:
-                    file_object.dict_bits[item.tag] += [bit_object]
-                else:
-                    file_object.dict_bits[item.tag] = [bit_object]
-         list_of_objects[i] = file_object
+                    if item.tag in file_object.dict_bits:
+                        file_object.dict_bits[item.tag] += [bit_object]
+                    else:
+                        file_object.dict_bits[item.tag] = [bit_object]
+            list_of_objects[i] = file_object
 
-       return list_of_objects
+        return list_of_objects
 
-object1 = ConfigModule()
-print(object1.get_list_objects(["rrbvp.xml"]))
+
+#object1 = ConfigModule()
+#print(object1.get_list_objects(["rxbsp.xml"]))
 # print(object1.get_dict_with_properties('rxbsp.xml'))
 # print(object1.get_name_key_mo('rxcap.xml'))
 # print(object1.get_bits_value())
