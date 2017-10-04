cd7a97350f28ee68387499cb0321ed8e760a499f
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-04 09:06:23 +0300

    make table

--- Controler.py
+++ Controler.py
@@ -23,10 +23,8 @@ class Controller:
         self.print_reader = PrintReader(text, self.xml_files)
         xml_objects = self.xml_reader.get_list_objects(self.print_reader.get_config_files_in_text())
         self.print_reader.make_subjects(xml_objects)
-        x = self.print_reader.subjects[0].get_subject_in_table()
-        y = x.get_values_from_row
-        for i in x:
-            print(i)
+        # for subject in self.print_reader.subjects:
+        #     print(str(subject))
         # print(list)
         # for key, val in list.items():
         #     print(key + "$", val)
@@ -48,27 +46,15 @@ class Controller:
     #     for row in table.table:
     #         print(table.get_values_from_row(row, xml_active_keys))
     #
-    def checker_bits(self,printout_bits=None, type_file = None,config_bits=None, norm_val = None):
-        if type_file.isdigit:
-            x=int(str(printout_bits), int(type_file))
-            text = (format(x,"0>10b"))
-           # text = text[0:].reverse()
-            print(text)
-            for i in range(len(text)-1,0,-1):
-               if text[i]!=str(norm_val):
-                   print(i)
-
-
-        else:
-            print("String")
+    # # def checker_bits(self,printout_bits=None,config_bits=None):
+    # #     x=int(str(printout_bits), 2)
+    # #     print(x)
 
 
 controller = Controller()
 # controller.checker_bits("10001")
 # on button click
-#controller.check_text()
-test = ConfigModule()
-controller.checker_bits("04", "16",test.get_list_objects(['rxbsp.xml']),0)
+controller.check_text()
 # if controller.no_subjects():
 #   print("Can't find eny subject to read")
 #    exit(0)
--- PrintReader.py
+++ PrintReader.py
@@ -1,5 +1,5 @@
 
-from re import match, findall, split
+from re import match, findall, split, finditer
 from re import sub
 
 
@@ -32,14 +32,29 @@ class PrintReader:
 class Subject:
     xml_instance = None
     text = None
-    table = None
+    tables = None
+    file_name = None
 
     def __init__(self, text, xml_objects):
         self.text = text
-        self.table = Table(text)
         for file_name, xml_obj in xml_objects.items():
             if match(xml_obj.root_limiter, text):
                 self.xml_instance = xml_obj
+                self.file_name = file_name
+        print("####################subject####################" + self.file_name)
+        lines = split("\n{2,}", text)
+        self.tables = []
+        self._make_tables(lines)
+
+        print(self.tables[0].get_values(0, "NSEI"))
+
+    def _make_tables(self, lines):
+        for line in lines:
+            if line:
+                self.tables += [Table(line)]
+
+    def get_name_key(self):
+        return self.xml_instance.name_key
 
     def has_key(self, key):
         return key in self.text
@@ -60,68 +75,85 @@ class Subject:
 
 
 class Table:
-    header_row = []
-    header_index = None
-    table = [[]]
+    header_row_with_start = None
+    table = None
+    current_row = 0
+    table_end = False
 
     def __init__(self, text):
-        lines = split("\n+", text)
-        self.header_index = []
-        self.header_row = self._make_header(lines[0], self.header_index)
-        self.table = self._make_table(lines[1:], self.header_index)
+        print("==============================table==============================")
+        print(text)
 
-    def _make_header(self, line, header_index):
-        array = split(" +", line)
+        lines = split("\n", text)
+        self.header_row_with_start = dict()
+        self.table = []
+        self._set_header(lines[0])
+        self._set_table(lines[1:])
 
-        for i in range(len(array)):
-            header_index += [line.find(array[i])]
-            array[i] = array[i].strip()
-        return array
+# create table
+    def _set_header(self, line):
+        header_arr = finditer("[\w:'-]+", line)
+        for column in header_arr:
+            self.header_row_with_start[column.group(0)] = column.start()
 
-    def _make_table(self, lines, header_index):
-        table = []
-
+    def _set_table(self, lines):
         for line in lines:
-            table += [self._make_row(line, header_index)]
-        return table
+            self.table += [self._make_row(line)]
 
-    def _make_row(self, line, header_index):
+    def _make_row(self, line):
+        column_start = list(self.header_row_with_start.values())
+        column_start_len = len(column_start)
         row = []
-        value = None
-        index_len = len(header_index)
-        end_i = 0
 
-        # find columns in the row
-        for i in range(index_len):
-            if i < index_len - 1:
-                end_i = header_index[i + 1]
+        for i in range(column_start_len):
+            if i + 1 < column_start_len:
+                cell = line[column_start[i]:column_start[i + 1]]
             else:
-                end_i = len(line)
-            value = line[header_index[i]:end_i]
-            row += [value.strip()]
+                cell = line[column_start[i]:]
+            row += [cell.strip()]
         return row
+# end create table
 
-    def get_number_of_rows(self):
-        return len(self.table)
+    def get_values(self, row_nbr, column, start="0,0", end="0,0"):
+        column_nbr = list(self.header_row_with_start.keys()).index(column)
+        start_coor = self._coor_to_int(start)
+        end_coor = self._coor_to_int(end)
+        return self._value_in(row_nbr, column_nbr, start_coor, end_coor)
 
-    def get_values_from_row(self, row, columns_names):
-        active_val = []
-        columns_num = None
+    def _value_in(self, row, column, start, end):
+        if start
 
-        for name, value_as in columns_names.items():
-            column_num = self.header_row.index(name)
-            active_val += [{name: self._get_value_from(row, column_num, value_as)}]
-        return active_val
+    def _get_value(self, column_nbr, row_nbr):
+        try:
+            return self.table[row_nbr][column_nbr]
+        except IndexError:
+            return None
 
-    def _get_value_from(self, row, column_num, read_as):
-        if read_as == "str":
-            return row[column_num]
-        if read_as.isdigit():
-            return int(row[column_num] or "0", int(read_as))
+    def _coor_to_int(self, coor):
+        coor_arr = split(coor)
+        return [int(coor[0]), int(coor[1])]
 
-    def get_string_by_column_name(self, name, row_num):
-        column_num = self.header_row.index(name)
-        return self._get_value_from(self.table[row_num], column_num, "str")
+    def get_name(self):
+        return " ".join(self.header_row_with_start.keys())
 
+    def is_this_table(self, pattern):
+        return match(pattern, self.get_name())
 
+    def get_next_row(self, xml_obj):
+        row_nbr = self.current_row
+        self.current_row = self._get_next_row_nbr(row_nbr, xml_obj.name_key)
+        if self.current_row == row_nbr:
+            self.table_end = True
 
+    def _row_to_dict(self, row_nbr, list_of_obj_keys):
+        row_dict = dict()
+        for key_obj in list_of_obj_keys:
+            row_dict[key_obj.name] = self.get_values(row_nbr, key_obj.name, start=key_obj.start, end=key_obj.end)
+
+    def _get_next_row_nbr(self, old_row_nbr, name_key):
+        new_row_nbr = old_row_nbr
+        while new_row_nbr < len(self.table):
+            new_row_nbr += 1
+            if self.get_values(new_row_nbr, name_key):
+                return new_row_nbr
+        return old_row_nbr
