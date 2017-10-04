d5a3850d7cf8954bec525e8a607c8c852216670e
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-04 11:41:58 +0300

    make table

--- Controler.py
+++ Controler.py
@@ -1,5 +1,5 @@
 from tkinter import filedialog
-# from PrintReader import PrintReader
+from PrintReader import PrintReader
 from XmlModule import ConfigModule
 
 
@@ -20,7 +20,7 @@ class Controller:
         text = file_name.read()
 
         self.xml_reader = ConfigModule()
-        # self.print_reader = PrintReader(text, self.xml_files)
+        self.print_reader = PrintReader(text, self.xml_files)
         xml_objects = self.xml_reader.get_list_objects(self.print_reader.get_config_files_in_text())
         self.print_reader.make_subjects(xml_objects)
         # for subject in self.print_reader.subjects:
@@ -50,51 +50,31 @@ class Controller:
     # #     x=int(str(printout_bits), 2)
     # #     print(x)
 
-    def checker_bits(self, printout_bits=None, config_bits=None):
-
-        for file in config_bits:
-            keys = config_bits.get("rxcap.xml")
-            for index in range(len(keys.list_of_object_keys)):
-                bits = keys.list_of_object_keys[index]
-                bit = bits.dict_bits
-                print_out_for_view = []
-                # print(keys)
-                # print(bits)
-                # print(bits.type)
-                # print(bits.norm_val)
-                # print(bits.name)
-                # print(keys.name_key)
-                # print(bit)
-                if bits.type.isdigit:
-                    x = int(str(printout_bits.get(bits.name)), int(bits.type))
-                    text = (format(x, "0>42b"))
-                    text_revers = text[::-1]
-                    print(text_revers)
-                    for i in range(0,len(text_revers)):
-                        if text_revers[i] != str(bits.norm_val):
-                            try:
-                                print_out_for_view.append(bit[i])
-                                print_out_for_view.append(keys.name_key)
-
-                            except IndexError:
-                                pass
-                        else:
-                            continue
-
-                    print (print_out_for_view)
-                else:
-                    print("String")
-
-
+#     def checker_bits(self,printout_bits=None, config_bits=None):
+#             keys =  config_bits.get("rxbsp.xml")
+#             bits     = keys.list_of_object_keys[0]
+#             bit = bits.dict_bits
+#             if type_file.isdigit:
+#                 x=int(str(printout_bits), int(type_file))
+#                 text = (format(x,"0>42b"))
+#                 text_revers = text[::-1]
+#                 print(text_revers)
+#                 for i in range(len(text_revers)):
+#                    if text_revers[i]!=str(norm_val):
+#                        print(bit[i-1])
+#
+#             else:
+#                 print("String")
+#
+#
 controller = Controller()
-# controller.checker_bits("10001")
-# on button click
-# controller.check_text()
-test = ConfigModule()
-controller.checker_bits({"OMLF1": "F",
-                         "OMLF2": "0",
-                         "RSLF1": "F"}, test.get_list_objects(['rxcap.xml']))
-# if controller.no_subjects():
-#   print("Can't find eny subject to read")
+# # controller.checker_bits("10001")
+# # on button click
+controller.check_text()
+# test = ConfigModule()
+# controller.checker_bits([{"OPCONDMAP":"04"}],test.get_list_objects(['rxbsp.xml']))
+# # if controller.no_subjects():
+# #   print("Can't find eny subject to read")
 #    exit(0)
 # end on button click
+
