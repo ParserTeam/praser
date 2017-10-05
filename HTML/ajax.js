44196192b23c0eb04b0cde2986dc209fbeb3f756
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-05 12:05:04 +0300

    Print for output_inf was changed

--- HTML/ajax.js
+++ HTML/ajax.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
     $("#btn").click(
 		function(){
-			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+			sendAjaxForm('result_form', 'ajax_form', 'interface.py');
 			return false; 
 		}
 	);
--- HTML/cgi-bin/interface.py
+++ HTML/cgi-bin/interface.py
@@ -30,38 +30,37 @@ text =  controller.check_text(get_input_inf())
 
 def output_inf(output):
     print("Content-type:text/html\r\n\r\n")
-    print()
-    print("<html>")
-    print("<head>")
-    print("<TITLE>ERRORS PARSER</TITLE>")
-    print("</head>")
-    print("<BODY STYLE = 'color: #000080' 'background-color: #3366CC'>")
-    print("<H1 STYLE = 'text-align: center'><STRONG>BSC ERRORS PARSER</STRONG></H1>")
-    print("<TABLE>")
-    print("<TBODY>")
-    print("<DIV STYLE = 'text-align: center' 'height = 100%' >")
-    print("<TR>")
-    print("<form name='pars'>")
-    print("<p STYLE = 'text-align: center'>Paste your log file here<Br>")
-    print("<textarea name='comment' cols='200' rows='15'></textarea></p>")
-    print(
-        "<p><input name = 'inf' type='submit' value='Pars' formaction = '/cgi-bin/interface.py' formmethod = 'post' ></input><input type='reset' value='Clean'></p>")
-    print("<p STYLE = 'text-align: center' >Errors:</p>")
-    print("</form>")
-    print(output)
-    print("</TR>")
-    print("</DIV>")
-    print("</TBODY>")
-    print("<TFOOT>")
-    print("<TR>")
-    print("<TD>")
-    print("<ADDRESS> HERE WILL BE CONTAKT INFORMATION</ADDRESS>")
-    print("</TD>")
-    print("</TR>")
-    print("</TFOOT>")
-    print("</TABLE>")
-    print("</body>")
-    print("</html>")
+    print("""
+        <html>
+        <head>
+        <TITLE>ERRORS PARSER</TITLE>
+        </head>
+        <BODY STYLE = 'color: #000080' 'background-color: #3366CC'>
+        <H1 STYLE = 'text-align: center'><STRONG>BSC ERRORS PARSER</STRONG></H1>
+        <TABLE>
+        <TBODY>
+        <DIV STYLE = 'text-align: center' 'height = 100%' >
+        <TR>
+        <form name='pars'>
+        <p STYLE = 'text-align: center'>Paste your log file here<Br>
+        <textarea name='comment' cols='200' rows='15'></textarea></p>
+        <p><input name = 'inf' type='submit' value='Pars' formaction = '/cgi-bin/interface.py' formmethod = 'post' ></input><input type='reset' value='Clean'></p>
+        <p STYLE = 'text-align: center' >Errors:</p>
+        </form>
+        {}
+        </TR>
+        </DIV>
+        </TBODY>
+        <TFOOT>
+        <TR>
+        <TD>
+        <ADDRESS> HERE WILL BE CONTAKT INFORMATION</ADDRESS>
+        </TD>
+        </TR>
+        </TFOOT>
+        </TABLE>
+        </body>
+        </html>""".format(output))
 
 
 output_inf(text)
