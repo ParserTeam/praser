f7e22df5b0bd1f0a5ccdc421baf85c496ecbeb86
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-05 16:56:55 +0300

    add end crutch

--- HTML/cgi-bin/PrintReader.py
+++ HTML/cgi-bin/PrintReader.py
@@ -23,10 +23,10 @@ class PrintReader:
 
     def make_subjects(self, xml_objects):
         limiters = [xml_obj.root_limiter for xml_obj in xml_objects.values()]
-        subject_texts = findall("|".join(limiters), self.text + "\n\n")
-        file = "C:/Users/ezburde/Desktop/rxbsp1.txt"
-        file = open(file, "r")
-        self.text = file.read()
+        # subject_texts = findall("|".join(limiters), self.text + "\n\n")
+        # file = "C:/Users/ezburde/Desktop/rxbsp1.txt"
+        # file = open(file, "r")
+        # self.text = file.read()
         subject_texts = findall("|".join(limiters), self.text + "\n\n")
         for sub_text in subject_texts:
             self.subjects += [Subject(sub_text, xml_objects)]
