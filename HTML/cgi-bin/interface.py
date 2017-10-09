83cc52936f38e82af691258bb3a0bbd0e9d803bf
ezglayu <YuniyOrelTeam@gmail.com>
2017-10-09 08:15:46 +0300

    ADD TABLE FOR OUTPUT HTML

--- HTML/cgi-bin/interface.py
+++ HTML/cgi-bin/interface.py
@@ -61,29 +61,61 @@ def output_inf(output):
         </html>""".format(output))
 
     a = output
-
-    for i in a:
-        print("-"*200+"<p></p>")
-        print(i[:i.find(".")] + "<p></p>")
-        for j in a.get(i):
-            b = j
-            print("-" * 200 + "<p></p>")
-            for z in b:
-                if type(b.get(z)) != dict:
-                    print(z)
-                    print(b.get(z) + "<p></p>")
-                else:
-                    if len(b.get(z)) != 0:
-                        print(z + "<p></p>")
-                        print("-" * 200 + "<p></p>")
-                        for y in b.get(z):
-                            print(y)
-                            print(b.get(z).get(y) + "<p></p>")
-                        print("-" * 200 + "<p></p>")
-
+    if len(a) != 0 :
+        print('''
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+            <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/bootstrap-table.min.css'>
+            <link rel='stylesheet prefetch' href='http://rawgit.com/vitalets/x-editable/master/dist/bootstrap3-editable/css/bootstrap-editable.css'>
+            <div class="container">
+			 <table class="table table-bordered">
+        ''')
+        for i in a:
+            print('''
+                <tr>
+					<th colspan="4" >''' + i[:i.find(".")].upper() + '''</th>
+				</tr>
+            ''')
+            for j in a.get(i):
+                b = j
+                print('''
+                    <tr>
+                            <th colspan="4" ></th>
+                    </tr>
+                ''')
+                for z in b:
+                    if type(b.get(z)) != dict:
+                        print('''
+                            <tr class="bg-primary">
+                              <th>''' + z + ''' -''' + '''</th>
+                              <th colspan="3" >''' + b.get(z) + ''' ;''' + '''</th>
+                            </tr>
+                        ''')
                     else:
-                        print(z)
-                        print(" OK<p></p>")
-
+                        if len(b.get(z)) != 0:
+                            print('''
+                                <tr class="bg-success ">
+                                  <th>''' + z + ''' :''' +'''</th>
+                                  <th colspan="3" >ERRORS:</th>
+                                </tr>
+                            ''')
+                            for y in b.get(z):
+                                print('''
+                                    <tr class="bg-success ">
+                                      <th>''' + y + ''' -''' + '''</th>
+                                      <th colspan="3" >''' + b.get(z).get(y) + ''' ;''' + '''</th>
+                                    </tr>
+                                ''')
+                        else:
+                            print('''
+                                <tr class="bg-success ">
+                                  <th>''' + z + ''' -''' + '''</th>
+                                  <th colspan="3" >OK;</th>
+                                </tr>
+                            ''')
+        print('''
+                    </table>
+		    </div>
+                ''')
 
 output_inf(text)
