3813f32a909f7c642a7405d78df052eacf5251ec
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-10 11:07:39 +0300

    add function for select version of xml

--- HTML/cgi-bin/interface.py
+++ HTML/cgi-bin/interface.py
@@ -27,8 +27,8 @@ text = controller.check_text(get_input_inf())
 # output = ['new inf']
 
 def output_inf(output):
-    print("Content-type:text/html\r\n\r\n")
-    print("""
+    print "Content-type:text/html\r\n\r\n"
+    print """
         <html>
         <head>
         <TITLE>ERRORS PARSER</TITLE>
@@ -58,73 +58,78 @@ def output_inf(output):
         </TFOOT>
         </TABLE>
         </body>
-        </html>""".format(output))
+        </html>""".format(output)
 
     a = output
-    if type(a) != str:
-        if len(a) != 0 :
-            print('''
-                <link rel="stylesheet" type="text/css" href="style.css">
-                <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
-                <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/bootstrap-table.min.css'>
-                <link rel='stylesheet prefetch' href='http://rawgit.com/vitalets/x-editable/master/dist/bootstrap3-editable/css/bootstrap-editable.css'>
-                <div class="container">
-                 <table class="table table-bordered">
-            ''')
-            for i in a:
-                print('''
-                                    <tr>
-                                            <th colspan="4" ></th>
-                                    </tr>
-                                ''')
-                print('''
-                    <tr>
-                        <th colspan="4" >''' + i[:i.find(".")].upper() + '''</th>
-                    </tr>
-                ''')
-                for j in a.get(i):
-                    b = j
-                    print('''
-                        <tr>
-                                <th colspan="4" ></th>
-                        </tr>
-                    ''')
-                    for z in b:
-                        if type(b.get(z)) != dict:
-                            print('''
-                                <tr class="bg-primary">
-                                  <th>''' + z + ''' -''' + '''</th>
-                                  <th colspan="3" >''' + b.get(z) + ''' ;''' + '''</th>
+    print type(a)
+
+    if len(a) != 0:
+        print '''
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+            <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.0/bootstrap-table.min.css'>
+            <link rel='stylesheet prefetch' href='http://rawgit.com/vitalets/x-editable/master/dist/bootstrap3-editable/css/bootstrap-editable.css'>
+            <div class="container">
+             <table class="table table-bordered">
+        '''
+        for i in a:
+            print '''
+                                <tr>
+                                        <th colspan="4" ></th>
                                 </tr>
-                            ''')
+                            '''
+            print '''
+                <tr>
+                    <th colspan="4" >''' + i[:i.find(".")].upper() + '''</th>
+                </tr>
+            '''
+            for j in a.get(i):
+                b = j
+                print '''
+                    <tr>
+                            <th colspan="4" ></th>
+                    </tr>
+                '''
+                for z in b:
+                    if type(b.get(z)) != dict:
+                        print '''
+                            <tr class="bg-primary">
+                              <th>''' + z + ''' -''' + '''</th>
+                              <th colspan="3" >''' + b.get(z) + ''' ;''' + '''</th>
+                            </tr>
+                        '''
+                    else:
+                        if len(b.get(z)) != 0:
+                            print '''
+                                <tr class="bg-success ">
+                                  <th>''' + z + ''' :''' +'''</th>
+                                  <th colspan="3" >ERRORS:</th>
+                                </tr>
+                            '''
+                            for y in b.get(z):
+                                print '''
+                                    <tr class="bg-success ">
+                                      <th>''' + y + ''' -''' + '''</th>
+                                      <th colspan="3" >''' + b.get(z).get(y) + ''' ;''' + '''</th>
+                                    </tr>
+                                '''
                         else:
-                            if len(b.get(z)) != 0:
-                                print('''
-                                    <tr class="bg-success ">
-                                      <th>''' + z + ''' :''' +'''</th>
-                                      <th colspan="3" >ERRORS:</th>
-                                    </tr>
-                                ''')
-                                for y in b.get(z):
-                                    print('''
-                                        <tr class="bg-success ">
-                                          <th>''' + y + ''' -''' + '''</th>
-                                          <th colspan="3" >''' + b.get(z).get(y) + ''' ;''' + '''</th>
-                                        </tr>
-                                    ''')
-                            else:
-                                print('''
-                                    <tr class="bg-success ">
-                                      <th>''' + z + ''' -''' + '''</th>
-                                      <th colspan="3" >OK;</th>
-                                    </tr>
-                                ''')
-            print('''
-                        </table>
-                </div>
-                    ''')
-    else:
-        print(a)
+                            print'''
+                                <tr class="bg-success ">
+                                  <th>''' + z + ''' -''' + '''</th>
+                                  <th colspan="3" >OK;</th>
+                                </tr>
+                            '''
+        print '''
+                    </table>
+            </div>
+                '''
+def select_version(L, Text):
+    """:return L(random object)"""
+    import random
+    secure_random = random.SystemRandom()
+    rand = secure_random.choice(L)
+    return [rand]
 
 output_inf(text)
 
