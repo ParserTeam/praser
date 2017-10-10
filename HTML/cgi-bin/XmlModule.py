5c80022f89d7d284a2e9db8ba29783431f51a57b
ezlazmy <YuniyOrelTeam@gmail.com>
2017-10-10 11:24:50 +0300

    waiting for Denis)

--- HTML/cgi-bin/XmlModule.py
+++ HTML/cgi-bin/XmlModule.py
@@ -1,6 +1,7 @@
 import xml.etree.cElementTree as ET
 from xml.etree.cElementTree import ParseError
-from interface import select_version
+import interface
+#from interface import select_version
 
 
 class KeysObject:
@@ -89,65 +90,65 @@ class ConfigModule(object):
 
 
 # function that return keys with their types
-def get_list_objects(self, true_config=None):
-    list_of_objects = {}
-    # run by list of files
-    for obj in range(len(true_config)):
-        if len(true_config[obj].file_names) > 2:
-            true_config[obj].file_names = select_version(true_config[obj].file_names, true_config[obj].text)
+    def get_list_objects(self, true_config=None):
+        list_of_objects = {}
+        # run by list of files
+        for obj in range(len(true_config)):
+            if len(true_config[obj].file_names) > 2:
+                true_config[obj].file_names = interface.select_version(true_config[obj].file_names, true_config[obj].text)
 
-    for i in true_config:
-        # create a empty object of file
-        file_object = ConfigObject()
-        # open xml config file
-        try:
-            tree = ET.ElementTree(file='cgi-bin\config\\' + i.file_names[0])
-        except (WindowsError, IOError):
-            tree = ET.ElementTree(file='config\\' + i.file_names[0])
-        # get header from config.xml
-        file_object.name_of_CANDY = i
+        for i in true_config:
+            # create a empty object of file
+            file_object = ConfigObject()
+            # open xml config file
+            try:
+                tree = ET.ElementTree(file='cgi-bin\config\\' + i.file_names[0])
+            except (WindowsError, IOError):
+                tree = ET.ElementTree(file='config\\' + i.file_names[0])
+            # get header from config.xml
+            file_object.name_of_CANDY = i.file_names[0]
 
-        file_object.header = tree.find('HEDER').text
-        # get name of key from config.xml
-        file_object.name_key = tree.find('NAME_KEY').text
-        # get regular expression from config.xml
-        # file_object.root_limiter = tree.getroot().attrib.get("limiter")
-        # get regular expression from config.xml
-        file_object.active_key_limiter = tree.find('ACTIVE_KEYS').attrib.get("limiter")
-        # get keys which we will printout from config.xml
-        file_object.list_of_keys_to_print = tree.find('PRINT_KEYS').text
-        # run by xml file
-        for item in tree.iterfind('.ACTIVE_KEYS/'):
-            # create object of class KeysObject
-            key_object = KeysObject()
-            key_object.name = item.tag
-            # get type of notation
-            key_object.type = (item.attrib).get("type")
-            # get start
-            key_object.start = (item.attrib).get("start")
-            # get end
-            key_object.end = (item.attrib).get("end")
-            # get value of bits
-            key_object.norm_val = (item.attrib).get("norm_val")
-            # run by keys sub tags
-            # print('###########')
-            for bits in item:
-                # create object of bit
-                bit_object = BitsObject()
-                # get name of bit
-                bit_object.name = bits.tag
-                # get description of bit
-                bit_object.text_of_bit = bits.text
-                # get value of bit
-                bit_object.value = bits.attrib.get("bit")
-                # magic
-                key_object.dict_bits += [bit_object]
-            file_object.list_of_object_keys.append(key_object)
-        # add file object to list
-        list_of_objects[i] = file_object
-    # return list of objects
-    return list_of_objects
-
-
-object1 = ConfigModule()
-print(object1.get_keys_from_files())
+            file_object.header = tree.find('HEDER').text
+            # get name of key from config.xml
+            file_object.name_key = tree.find('NAME_KEY').text
+            # get regular expression from config.xml
+            # file_object.root_limiter = tree.getroot().attrib.get("limiter")
+            # get regular expression from config.xml
+            file_object.active_key_limiter = tree.find('ACTIVE_KEYS').attrib.get("limiter")
+            # get keys which we will printout from config.xml
+            file_object.list_of_keys_to_print = tree.find('PRINT_KEYS').text
+            # run by xml file
+            for item in tree.iterfind('.ACTIVE_KEYS/'):
+                # create object of class KeysObject
+                key_object = KeysObject()
+                key_object.name = item.tag
+                # get type of notation
+                key_object.type = (item.attrib).get("type")
+                # get start
+                key_object.start = (item.attrib).get("start")
+                # get end
+                key_object.end = (item.attrib).get("end")
+                # get value of bits
+                key_object.norm_val = (item.attrib).get("norm_val")
+                # run by keys sub tags
+                # print('###########')
+                for bits in item:
+                    # create object of bit
+                    bit_object = BitsObject()
+                    # get name of bit
+                    bit_object.name = bits.tag
+                    # get description of bit
+                    bit_object.text_of_bit = bits.text
+                    # get value of bit
+                    bit_object.value = bits.attrib.get("bit")
+                    # magic
+                    key_object.dict_bits += [bit_object]
+                file_object.list_of_object_keys.append(key_object)
+            # add file object to list
+            list_of_objects[i.file_names[0]] = file_object
+        # return list of objects
+        return list_of_objects
+#
+# #
+# object1 = ConfigModule()
+# print(object1.get_list_objects())
