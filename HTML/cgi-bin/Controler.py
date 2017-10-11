be35ee621d577241972d922d6c35f634065b38bd
ParserTeam <YuniyOrelTeam@gmail.com>
2017-10-11 09:57:35 +0300

    Alpha 1.5 add MML parser to PrintReader

--- HTML/cgi-bin/Controler.py
+++ HTML/cgi-bin/Controler.py
@@ -19,6 +19,7 @@ class Controller:
             return "<b>No file found for text</b><p>Files available: " + ", ".join(self.xml_files.keys()) + "</p>"
         self._check_file_version(self.print_reader.subjects)
         list_check_values = self.print_reader.get_check_values()
+        # return list_check_values
         return self.create_data_for_out(list_check_values)
 
     def create_data_for_out(self, list_check_values):
@@ -90,15 +91,15 @@ class Controller:
 if __name__ == "__main__":
     controller = Controller()
 
-    input_text = get_input_inf()
+    # input_text = get_input_inf()
 
-    # from tkFileDialog import askopenfile
-    # input_text = askopenfile("r").read()
+    from tkFileDialog import askopenfile
+    input_text = askopenfile("r").read()
 
     text = controller.check_text(input_text)
 
-    # print text
-    output_inf(text)
+    print text
+    # output_inf(text)
 
 
 # if __name__ == "__main__":
