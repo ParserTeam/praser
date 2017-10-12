017c7818eadd5d15ef444101281705e292fcbb4a
ezanaho <YuniyOrelTeam@gmail.com>
2017-10-12 15:22:59 +0300

    change style

--- HTML/cgi-bin/xmlinfo.py
+++ HTML/cgi-bin/xmlinfo.py
@@ -1,23 +1,26 @@
+import cgi, cgitb
+
+# fullname = os.path.join(".\config", name)  # take name
+# if os.path.isfile(fullname):
 import os
-
-print '''
-        <html>
-        <head>
-        <TITLE>ERRORS PARSER</TITLE>
-        <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css">
-        </head>
-        <body>
-'''
-names = os.listdir("./config")
-for name in names:
-    fullname = os.path.join(".\config", name)  # take name
-    if os.path.isfile(fullname):
-        print "<DIV id = 'navigationxml'><nav><a href=/config/" + name + ">" + fullname + "</a></nav>"
-print '''
-<p>&nbsp</p>
-<nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav>
-</div>
-        </body>
-        </html>
-
-'''
+def xmlinfo():
+	print '''
+			<html>
+	        <head>
+	        <TITLE>ERRORS PARSER</TITLE>
+	        <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css">
+	        </head>
+			<body>
+			<div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
+			<DIV id = 'navigationxml'>
+	'''
+	names = os.listdir(".\config")
+	for name in names:
+		print "<nav><a href=/config/" + name +">" + name + "</a></nav>"
+	print '''
+	</div>
+		    </body>
+	        </html>
+	
+	'''
+output = xmlinfo()
