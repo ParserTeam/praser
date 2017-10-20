595f3b02a53e952be172d444033ba29fc15756e2
ezanaho <YuniyOrelTeam@gmail.com>
2017-10-20 15:27:34 +0300

    change style page xmlinfo

--- HTML/cgi-bin/xmlinfo.py
+++ HTML/cgi-bin/xmlinfo.py
@@ -10,38 +10,38 @@ def xmlinfo(): # use it to output and open config files, criate html page(action
 	        <head>
 	        <TITLE>PARSER</TITLE>
 	        <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css"> 
-	        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-			<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
-
 	        </head>
 			<body>
 			<div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
 			<DIV id = 'navigationxml'>
+			<div STYlE='width:40%; float:left; clear: both;height:0px;'>
 	'''
 	names = os.listdir(".\config")
 	i = 0
+
 	for name in names:
 		i +=1
 		print "<nav><span>"+str(i)+" - "+"</span><a href=/config/" + name +">" + name + "</a></nav>"
-		names = os.listdir(".\config")
-		if name == "versions.txt":
-			print '''
-			<form name='t2' method='post' id = 'ajax_form'>
-			<input type='text' name='txt' >  <br/>
-			<input type='submit' value='Add new release' id = 'btn2' formaction='releaseAdd.py' />
-			<input type="reset" value="Clean"/>
-			</form>
-			<form name='t2' method='post' id = 'ajax_form'>
-			<input type='text' name='txtdel' >  <br/>
-			<input type='submit' value='Delete release' id = 'btn2' formaction='releaseAdd.py' />
-			<input type="reset" value="Clean"/>
-			</form>
-				'''
-	print '''
+	print '''</div>
 	</div>
-<p id="results"></p>
-		    </body>
-	        </html>
 	
 	'''
+	for name in names:
+		if name == "versions.txt":
+			print '''
+			<div STYlE='width:48%; float:right; text-align:right;clear: both;height:0px; padding-right:20px;'>
+			<form name='t2' method='post' id = 'ajax_form'>
+			<input type='text' name='txt' >  <br/>
+			<input type='submit' value='Add new release' formaction='releaseAdd.py' />
+			<input type="reset" value="Clean"/>
+			</form>
+			<p>&nbsp</p>
+			<form name='t2' method='post' id = 'ajax_form'>
+			<input type='text' name='txtdel' >  <br/>
+			<input type='submit' value='Delete release' formaction='releaseAdd.py' />
+			<input type="reset" value="Clean"/>
+			</form>
+			</div>
+		    </body>
+	        </html>'''
 output = xmlinfo()
