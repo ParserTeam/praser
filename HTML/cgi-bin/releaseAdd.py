e880e6e6d72e868e484423422ccbb484ffe2d0c3
ezanaho <YuniyOrelTeam@gmail.com>
2017-10-20 17:55:35 +0300

    change file releaseAdd, fix bag

--- HTML/cgi-bin/releaseAdd.py
+++ HTML/cgi-bin/releaseAdd.py
@@ -9,63 +9,32 @@ fdel  = form.getvalue('txtdel')
 
 
 if fdel == None and fadd == None:
-    file = open('.\config\\versions.txt', 'r')
-    print '''
-        <html>
-        <head>
-        <TITLE>PARSER</TITLE>
-        <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css">
-        </head>
-        <body>
-        <div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
-        <div>
-        <P>&#8195;You typed any version of release.</P>
-        <div>
-        <p STYLE="color:blue">&#8195;RELEASES:</p> '''
-    for line in file:
-        print line + ';'
-    file.close()
-    print '''
-        </body>
-        </html>
-        '''
+    text = "You typed any version of release."
 
 if fadd != None:
     file = open('.\config\\versions.txt','a')
     file.write('\n'+fadd)
     file.close()
     file = open('.\config\\versions.txt','r')
-    print '''
-    <html>
-    <head>
-    <TITLE>PARSER</TITLE>
-    <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css">
-    </head>
-    <body>
-    <div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
-    <div>
-    <P>&#8195;New version of release wos added successfully.</P>
-    <div>
-    <p STYLE="color:blue">&#8195;RELEASES: '''
-    for line in file:
-        print line +  ';'
-    file.close()
-    '''</p>'''
-    print '''
-    </body>
-    </html>
-        '''
+    text = "New version of release wos added successfully."
 
 if fdel != None:
-    fin = open('.\config\\versions.txt',)
-    text = fin.read()
-    lines = text.split("\n")
-    fin.close()
-    fout = open('.\config\\versions.txt', "w")
-    lines.remove(fdel)
-    fout.write("\n".join(lines))
-    fout.close()
-    print '''
+    fin = open('.\config\\versions.txt', )
+    for line in fin.readlines():
+        fin.close()
+        if fdel in line:
+            fin = open('.\config\\versions.txt',)
+            text = fin.read()
+            lines = text.split("\n")
+            fin.close()
+            fout = open('.\config\\versions.txt', "w")
+            lines.remove(fdel)
+            fout.write("\n".join(lines))
+            fout.close()
+            text = "Version of release wos deleted successfully."
+        else:
+            text = "There no such release version like: " + fdel + "."
+print '''
     <html>
     <head>
     <TITLE>PARSER</TITLE>
@@ -74,15 +43,15 @@ if fdel != None:
     <body>
     <div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
     <div>
-    <P>&#8195;Version of release wos deleted successfully.</P>
+    <P>&#8195;'''+text+'''</P>
     <div>
     <p STYLE="color:blue">&#8195;RELEASES: '''
-    file = open('.\config\\versions.txt', 'r')
-    for line in file:
-        print line +  ';'
-    file.close()
-    '''</p>'''
-    print '''
+file = open('.\config\\versions.txt', 'r')
+for line in file:
+    print line +  ';'
+file.close()
+'''</p>'''
+print '''
     </body>
     </html>
 '''
