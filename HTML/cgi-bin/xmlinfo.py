d64cff0b854815c41188e30952d6c2f409ca994d
ezanaho <YuniyOrelTeam@gmail.com>
2017-10-23 09:18:56 +0300

    change links

--- HTML/cgi-bin/xmlinfo.py
+++ HTML/cgi-bin/xmlinfo.py
@@ -9,10 +9,10 @@ def xmlinfo(): # use it to output and open config files, criate html page(action
 			<html>
 	        <head>
 	        <TITLE>PARSER</TITLE>
-	        <link rel="stylesheet" type="text/css" href="http://localhost:8000/style.css"> 
+	        <link rel="stylesheet" type="text/css" href="/style.css"> 
 	        </head>
 			<body>
-			<div id ="goback"><p>&nbsp</p><nav><a href="http://localhost:8000/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
+			<div id ="goback"><p>&nbsp</p><nav><a href="/">BACK TO PARSER</a></nav><p>&nbsp</p></div>
 			<DIV id = 'navigationxml'>
 			<div STYlE='width:40%; float:left; background:Lavender;clear: both;height:0px;'>
 	'''
